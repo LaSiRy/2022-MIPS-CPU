13d6fce50858bfa86802824afd4633b60c879440
LaSiRy <lishiyu_012@163.com>
2022-08-05 20:38:35 +0800

    finish_lab3

--- thinpad_top.srcs/sources_1/new/myCPU/HazardDetec.v
+++ thinpad_top.srcs/sources_1/new/myCPU/HazardDetec.v
@@ -81,11 +81,7 @@ module HazardDetec(//��ʵ����forwarding֮��ֻ��xxx-sd, xxx-l
         else if(preMEM_rd!=0&&IFID_rs2==preMEM_rd&&IFID_sw)begin
          data_hazard<=0;
          control_hazard<=0;//xxx-sw
-        end 
-        else if(EXpreMEM_Branch||preMEM_Branch)begin//
-         data_hazard<=0;
-         control_hazard<=1'b1;
-        end   
+        end  
         else if(EXpreMEM_addr >= 32'h80000000 && EXpreMEM_addr <= 32'h803fffff && (IDEX_MR|IDEX_MW))begin
          data_hazard<=0;
          control_hazard<=0;//structure hazard
@@ -94,6 +90,10 @@ module HazardDetec(//��ʵ����forwarding֮��ֻ��xxx-sd, xxx-l
          data_hazard<=0;
          control_hazard<=0;//structure hazard
         end
+        else if(EXpreMEM_Branch|preMEM_Branch)begin//
+         data_hazard<=0;
+         control_hazard<=1'b1;
+        end  
         else begin
          data_hazard<=1'b1;
          control_hazard<=1'b1;
